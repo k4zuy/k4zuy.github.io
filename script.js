639285c0ae384cb8ed2c1a6b99ac6b7b8fc95c1e
k4zuy <102243006+k4zuy@users.noreply.github.com>
2023-09-22 14:48:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -71,10 +71,68 @@ $(document).ready(function() {
     function createImageElement(imageName) {
         var imgElement = $('<img>').attr('src', 'images/' + imageName).attr('alt', imageName);
         
-        // ... [rest of the imgElement initialization code]
+        // Set random starting position and velocity for each image
+        var randomLeft = Math.random() * ($(window).width() - 100);  
+        var randomTop = Math.random() * ($(window).height() - 100); 
+        imgElement.css({
+            left: randomLeft + 'px',
+            top: randomTop + 'px',
+            position: 'absolute'
+        });
 
+        imgElement.data('velocity', {
+            dx: (Math.random() - 0.5) * 1,  // Slower movement
+            dy: (Math.random() - 0.5) * 1,
+            dz: (Math.random() - 0.5) * 0.05
+        });
+        
         return imgElement;
     }
 
-    // ... [rest of the code, e.g., updateVelocities, animateImages]
+    function updateVelocities() {
+        $(".image-container img").each(function() {
+            var velocity = $(this).data('velocity');
+            velocity.dx = (Math.random() - 0.5) * 1;  // Slower movement
+            velocity.dy = (Math.random() - 0.5) * 1;
+        });
+    }
+
+    function animateImages() {
+        $(".image-container img").each(function() {
+            var $img = $(this);
+            var pos = $img.position();
+            var velocity = $img.data('velocity');
+            
+            var newLeft = pos.left + velocity.dx;
+            var newTop = pos.top + velocity.dy;
+            
+            // Keep images within the viewport and reverse direction if it hits an edge
+            if (newLeft < 0 || newLeft > $(window).width() - $img.width()) {
+                velocity.dx = -velocity.dx;
+                newLeft = pos.left + velocity.dx;
+            }
+            if (newTop < 0 || newTop > $(window).height() - $img.height()) {
+                velocity.dy = -velocity.dy;
+                newTop = pos.top + velocity.dy;
+            }
+
+            // Implementing the 3D-like effect
+            var newZ = velocity.dz + ($img.data('z') || 1);
+            if (newZ < 0.5 || newZ > 1.5) {
+                velocity.dz = -velocity.dz;
+                newZ += velocity.dz;
+            }
+            var scaleValue = newZ;
+            $img.data('z', newZ);
+            $img.css('transform', 'scale(' + scaleValue + ')');
+            $img.css('z-index', Math.round(scaleValue * 100));
+
+            $img.css({
+                left: newLeft + 'px',
+                top: newTop + 'px'
+            });
+        });
+
+        requestAnimationFrame(animateImages);  // Keep the animation running
+    }
 });
